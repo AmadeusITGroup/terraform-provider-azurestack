a7835bb85c68d148f881efaefe03b77e7f123f2b
kt <kt@katbyte.me>
2022-01-17 09:35:12 -0800

    make genreate and pass enpoint in

--- .teamcity/components/build_azure.kt
+++ .teamcity/components/build_azure.kt
@@ -10,10 +10,10 @@ class ClientConfiguration(var clientId: String,
 class LocationConfiguration(var primary : String, var secondary : String, var ternary : String, var rotate : Boolean) {
 }
 
-fun ParametrizedWithType.ConfigureAzureSpecificTestParameters(endpoint: String, config: ClientConfiguration, locationsForEnv: LocationConfiguration) {
+fun ParametrizedWithType.ConfigureAzureSpecificTestParameters(config: ClientConfiguration, locationsForEnv: LocationConfiguration) {
     hiddenPasswordVariable("env.ARM_CLIENT_ID", config.clientId, "The ID of the Service Principal used for Testing")
     hiddenPasswordVariable("env.ARM_CLIENT_SECRET", config.clientSecret, "The Client Secret of the Service Principal used for Testing")
-    hiddenVariable("env.ARM_ENDPOINT", endpoint, "The Azure Stack Endpoint to use")
+    hiddenVariable("env.ARM_ENDPOINT", config.endpoint, "The Azure Stack Endpoint to use")
     hiddenPasswordVariable("env.ARM_SUBSCRIPTION_ID", config.subscriptionId, "The ID of the Azure Subscription used for Testing")
     hiddenPasswordVariable("env.ARM_TENANT_ID", config.tenantId, "The ID of the Azure Tenant used for Testing")
     hiddenVariable("env.ARM_TEST_LOCATION", locationsForEnv.primary, "The Primary region which should be used for testing")
